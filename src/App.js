7eb5080864f810c79bd56446f7703e837ee4a7a4
gaurav-kumar17 <gk170106@gmail.com>
2022-09-26 09:29:52 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -29,16 +29,20 @@ function App() {
             products.map((product, index) => {
               {/* console.log(product); */}
               return (
-                <div className="card" style={{ width: "18rem" }}>
-                  <img src={product.thumbnail} className="card-img-top" alt="..." />
-                  <div className="card-body">
-                    <h5 className="card-title">{product.title}</h5>
-                    <p className="card-text">
-                    {product.description}
-                    </p>
-                    <a href="#" className="btn btn-primary">
-                      Go somewhere
-                    </a>
+                <div key={index} className="col">
+                  <div className="card" style={{ width: "18rem" }}>
+                    <img
+                      src={product.thumbnail}
+                      className="card-img-top "
+                      alt="..."
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{product.title}</h5>
+                      <p className="card-text">{product.description}</p>
+                      {/* <a href="#" className="btn btn-primary">
+                  Go somewhere
+                </a> */}
+                    </div>
                   </div>
                 </div>
               );
