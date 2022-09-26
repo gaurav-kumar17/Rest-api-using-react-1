b328002c71110eb1d880fa2cc6c4b357233ca9e9
gaurav-kumar17 <gk170106@gmail.com>
2022-09-26 09:13:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch("https://dummyjson.com/products");
+      const data = await res.json();
+      console.log(data);
+      setProducts(data.products);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); //[] dene se sirf ek baar hi useEffect func. run karega
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <div className="row">
+          {products &&
+            products.map((product, index) => {
+              {/* console.log(product); */}
+              return (
+                <div className="card" style={{ width: "18rem" }}>
+                  <img src={product.thumbnail} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{product.title}</h5>
+                    <p className="card-text">
+                    {product.description}
+                    </p>
+                    <a href="#" className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </>
   );
 }
 
